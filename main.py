536e89dec66483b0656f2ce938e315c19762a650
glebkaaa <129597452+glebkaaa@users.noreply.github.com>
2023-04-22 17:27:10 +0600

    Create main.py

--- main.py
+++ main.py
@@ -1,41 +1,24 @@
 import discord
-from random import choice
+from discord.ext import commands
 from credits import bot_token
-from bot_logic import gen_pass
 
 intents = discord.Intents.default()
-intents.members = True
-intents.typing = True
-intents.presences = True
 intents.message_content = True
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hi!')
-    elif message.content.startswith('$bye'):
-        await message.channel.send('\\U0001f642')
-    # мой код доработки
-    elif message.content.startswith('$smile'):
-        smile_list = ['🐍', '🐲', '🐕']
-        await message.channel.send(choice(smile_list))
-    elif message.content.startswith('$play'):
-        play_list = ['🦅', '🪙']
-        await message.channel.send(choice(play_list))
-    elif message.content.startswith('$pass'):
-        await message.channel.send(gen_pass(10))
-    if message.author != client.user:
-        await message.channel.send(message.content)
-    else:
-        await message.channel.send(message.content)
+@bot.command()
+async def heh(ctx, count_heh=5):
+    await ctx.send('he' * count_heh)
 
-client.run(bot_token)
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    await ctx.send(left + right)
+
+bot.run(bot_token)
